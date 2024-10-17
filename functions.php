cb758331140937f7cda67a39e9da03a2f0a5bdb4
AlvaroRC718 <159561476+AlvaroRC718@users.noreply.github.com>
2024-10-17 02:29:53 +0200

    Add files via upload

--- functions.php
+++ functions.php
@@ -1,9 +1,15 @@
 <?php
+include "DBpokemon.php";
 function print_select_pokemon($array_pokemon)
 {
-    $array_pokemon_size = count($array_pokemon);
+    global $array_pokemon_size;
+    $pokemon_random = mt_rand(1, $array_pokemon_size - 1); //pongo pokemon aleatorios, el 0 lo oculto porque es un easter egg
 
     for ($i = 0; $i < $array_pokemon_size; $i++) {
-        echo "<option value='" . $array_pokemon[$i][1] . "' name='select_pokemon' id='" . $array_pokemon[$i][1] . "'>" . $array_pokemon[$i][1] . "</option>";
+        if ($i == $pokemon_random) { //selecciono un pordefecto aleatorio
+            echo "<option value='" . $array_pokemon[$i]["name"] . "' name='select_pokemon' id='" . $array_pokemon[$i]["name"] . "' selected>" . $array_pokemon[$i]["name"] . "</option>";
+        }else{
+            echo "<option value='" . $array_pokemon[$i]["name"] . "' name='select_pokemon' id='" . $array_pokemon[$i]["name"] . "'>" . $array_pokemon[$i]["name"] . "</option>";
+        }
     }
 }
